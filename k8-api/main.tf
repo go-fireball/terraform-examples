508068e99353b34d45699d774475233bddc3733f
go-fireball <126999224+go-fireball@users.noreply.github.com>
2023-03-24 17:14:26 -0500

    Update main.tf

--- k8-api/main.tf
+++ k8-api/main.tf
@@ -1,3 +1,4 @@
+# Configure the Kubernetes provider to connect to a local Kubernetes cluster using the specified context
 provider "kubernetes" {
   host                     = "https://localhost:6443"
   config_path              = "~/.kube/config"
@@ -5,6 +6,7 @@ provider "kubernetes" {
   config_context_cluster   = "docker-desktop"
 }
 
+# Define a Kubernetes deployment named "hello" running the "nginxdemos/hello" image
 resource "kubernetes_deployment" "hello" {
   metadata {
     name = "hello"
@@ -13,20 +15,24 @@ resource "kubernetes_deployment" "hello" {
   spec {
     replicas = 1
 
+    # Specify the label selector to match with the pod template
     selector {
       match_labels = {
         app = "hello"
       }
     }
 
+    # Define the pod template for the deployment
     template {
       metadata {
+        # Specify the labels to apply to the pod
         labels = {
           app = "hello"
         }
       }
 
       spec {
+        # Define the container specification for the pod
         container {
           image = "nginxdemos/hello"
           name  = "hello"
@@ -39,22 +45,25 @@ resource "kubernetes_deployment" "hello" {
   }
 }
 
-
+# Define a Kubernetes service named "hello" that routes traffic to the "hello" deployment
 resource "kubernetes_service" "hello" {
   metadata {
     name = "hello"
   }
 
   spec {
+    # Specify the label selector to match with the pods to be exposed by the service
     selector = {
       app = "hello"
     }
 
+    # Define the port specification for the service
     port {
       port        = 80
       target_port = 80
     }
 
+    # Specify that the service should be exposed as a NodePort
     type = "NodePort"
   }
 }
